e2cdc82b657fb303e3ab098dca1f1e4825e60daa
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-01-22 20:00:03 -0300

    Produtos adicionados

--- functions.php
+++ functions.php
@@ -1,3 +1,14 @@
 <?php
+function Tryvary_prefix_remove_default_images( $sizes ) {
+	unset( $sizes['thumbnail'] );
+	unset( $sizes['small'] );
+	unset( $sizes['medium'] );
+	unset( $sizes['large'] );
+	unset( $sizes['medium_large'] );
+	unset( $sizes['1536x1536'] );
+	unset( $sizes['2048x2048'] );
+	return $sizes;
+}
+add_filter( 'intermediate_image_sizes_advanced', 'Tryvary_prefix_remove_default_images' );
 
 ?>
